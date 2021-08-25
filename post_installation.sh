0b33b8d5c4e682bc15077383de5bd69ad447558f
antipiot <jonassauge@gmail.com>
2021-08-25 22:04:00 +0200

    Update post_installation.sh

--- post_installation.sh
+++ post_installation.sh
@@ -28,15 +28,15 @@ dbname=nextcloud
 dbhostname=db
 # mysqlrootpwd=$(LC_ALL=C tr -dc 'A-Za-z0-9!#%&\()*+,-./:;<=>?@[\]^_{}~' </dev/urandom | head -c 20)
 # mysqlnextcloudpwd=$(LC_ALL=C tr -dc 'A-Za-z0-9!#%&\()*+,-./:;<=>?@[\]^_{}~' </dev/urandom | head -c 20)
-mysqlrootpwd=tes
+mysqlrootpwd=test
 mysqlnextcloudpwd=test
 
 # Starting mysql container
-docker run -d --name $dbhostname --user $uid:$uid -v $rootfolder/database:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$mysqlrootpwd -e MYSQL_DATABASE=$dbname -e MYSQL_USER=$nextcloud -e MYSQL_PASSWORD=$mysqlnextcloudpwd mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+#docker run -d --name $dbhostname --user $uid:$uid -v $rootfolder/database:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$mysqlrootpwd -e MYSQL_DATABASE=$dbname -e MYSQL_USER=$nextcloud -e MYSQL_PASSWORD=$mysqlnextcloudpwd mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 # Starting nextcloud container
-docker run -d -p $https:443 -p $http:80 --name=nextcloud --link $dbhostname --restart unless-stopped -e TZ=Europe/Geneva -v $rootdatafolder/nextcoud/config:/config -v $rootdatafolder/nextcoud/data:/data -e MYSQL_DATABASE=$dbname -e MYSQL_HOST=$dbhostname-e MYSQL_USER=$nextcloud -e MYSQL_PASSWORD=$mysqlnextcloudpwd linuxserver/nextcloud
+#docker run -d -p $https:443 -p $http:80 --name=nextcloud --link $dbhostname --restart unless-stopped -e TZ=Europe/Geneva -v $rootdatafolder/nextcoud/config:/config -v $rootdatafolder/nextcoud/data:/data -e MYSQL_DATABASE=$dbname -e MYSQL_HOST=$dbhostname-e MYSQL_USER=$nextcloud -e MYSQL_PASSWORD=$mysqlnextcloudpwd linuxserver/nextcloud
 # Starting updater container
-docker run -d --name watchtower --restart=unless-stopped -e WATCHTOWER_SCHEDULE="0 0 4 * * *" -e WATCHTOWER_CLEANUP="true" -e TZ="Europe/paris" -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
+# docker run -d --name watchtower --restart=unless-stopped -e WATCHTOWER_SCHEDULE="0 0 4 * * *" -e WATCHTOWER_CLEANUP="true" -e TZ="Europe/paris" -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
 echo "Credentials:
 Database hostname: $dbhostname
 Database user: $dbusername
