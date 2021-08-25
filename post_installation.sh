771090ec3d75e5128416ec2fa7b527625391eb45
antipiot <jonassauge@gmail.com>
2021-08-25 22:13:02 +0200

    Update post_installation.sh

--- post_installation.sh
+++ post_installation.sh
@@ -4,7 +4,7 @@
 
 ## Installing sources, dependencies and updating host
 # Add Docker’s official GPG key:
-# curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # Setup Stable Docker repository
 # echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
