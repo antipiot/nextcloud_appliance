e931e4737c356c90d3a5c3004864f22704b0c13a
antipiot <jonassauge@gmail.com>
2021-08-25 22:06:29 +0200

    Update post_installation.sh

--- post_installation.sh
+++ post_installation.sh
@@ -4,7 +4,7 @@
 
 ## Installing sources, dependencies and updating host
 # Add Docker’s official GPG key:
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # Setup Stable Docker repository
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -14,7 +14,6 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] 
 
 apt update -y
 apt install -y net-tools gnupg2 curl ca-certificates docker-ce docker-ce-cli containerd.io
-apt dist-update -y
 
 ## Starting Nextcloud Installation
 # Creating environnmente variables
