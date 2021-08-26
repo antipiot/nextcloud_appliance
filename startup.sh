7a0d0b443e97cf5e6ded79c66430a96bb9c281d5
antipiot <jonassauge@gmail.com>
2021-08-26 07:46:06 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt update -y
-apt install -y net-tools docker-ce docker-ce-cli containerd.io
+apt install -y docker-ce docker-ce-cli containerd.io
 
 ## Starting Nextcloud Installation
 # Creating environnmente variables
