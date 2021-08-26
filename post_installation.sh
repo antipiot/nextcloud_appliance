22e027af87cb979b34a2a3fcafc859e56b985fbd
antipiot <jonassauge@gmail.com>
2021-08-26 07:22:38 +0200

    Update post_installation.sh

--- post_installation.sh
+++ post_installation.sh
@@ -7,7 +7,7 @@
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # Setup Stable Docker repository
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 echo "@reboot bash /tmp/startup.sh" > /etc/crontab
 # Install Docker along network tools
 
