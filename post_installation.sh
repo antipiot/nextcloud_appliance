9a069e2f131c5b36facbf5e7213849fffe57b6d2
antipiot <jonassauge@gmail.com>
2021-08-26 07:28:26 +0200

    Update post_installation.sh

--- post_installation.sh
+++ post_installation.sh
@@ -8,7 +8,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/
 
 # Setup Stable Docker repository
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-echo "@reboot bash /tmp/startup.sh" > /etc/crontab
+echo "@reboot bash /root/startup.sh" > /etc/crontab
 # Install Docker along network tools
 
 ## Starting Nextcloud Installation
