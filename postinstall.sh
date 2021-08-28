39cac13702117c40867979799ad7ef78aadd31a3
antipiot <jonassauge@gmail.com>
2021-08-28 16:59:57 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -9,28 +9,3 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] 
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 apt update -y
 apt install -y docker-ce docker-ce-cli containerd.io
-
-uid=0 
-gid=0
-rootfolder=/root/nextcloud
-http=80
-https=443
-dbusername=nextcloud
-dbname=nextcloud
-dbhostname=db
-mysqlrootpwd=$(LC_ALL=C tr -dc 'A-Za-z0-9!#%&\()*+,-./:;<=>?@[\]^_{}~' </dev/urandom | head -c 20)
-mysqlnextcloudpwd=$(LC_ALL=C tr -dc 'A-Za-z0-9!#%&\()*+,-./:;<=>?@[\]^_{}~' </dev/urandom | head -c 20)
-
-
-# Starting mysql container
-docker run -d --name $dbhostname --user $uid:$uid -v $rootfolder/nextcloud/database:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$mysqlrootpwd -e MYSQL_DATABASE=$dbname -e MYSQL_USER=$nextcloud -e MYSQL_PASSWORD=$mysqlnextcloudpwd mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
-# Starting nextcloud container
-docker run -d -p $https:443 -p $http:80 --name=nextcloud --link $dbhostname --restart unless-stopped -e TZ=Europe/Geneva -v $rootdatafolder/nextcloud/config:/config -v $rootdatafolder/nextcloud/data:/data -e MYSQL_DATABASE=$dbname -e MYSQL_HOST=$dbhostname -e MYSQL_USER=$nextcloud -e MYSQL_PASSWORD=$mysqlnextcloudpwd linuxserver/nextcloud
-# Starting updater container
-docker run -d --name watchtower --restart=unless-stopped -e WATCHTOWER_SCHEDULE="0 0 4 * * *" -e WATCHTOWER_CLEANUP="true" -e TZ="Europe/paris" -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
-echo "Database user: $dbusername \
-Database password: $mysqlnextcloudpwd \
-Database hostname: $dbhostname \
-Database name: $dbname \ 
-Database hostname: $dbhostname \
-Database root password: $mysqlnextcloudpwd" > $rootfolder/nextcloud/credentials.txt
