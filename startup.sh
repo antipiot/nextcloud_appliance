1ed4ca0d277e188a175f50e6347123ce33fe8c69
antipiot <jonassauge@gmail.com>
2021-08-28 19:01:23 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -18,9 +18,8 @@ https=443
 dbusername=nextcloud
 dbname=nextcloud
 dbhostname=db
-mysqlnextcloudpwd=test
 mysqlrootpwd=$(LC_ALL=C tr -dc 'A-Za-z0-9!#%&\()*+,-./:;<=>?@[\]^_{}~' </dev/urandom | head -c 20)
-#mysqlnextcloudpwd=$(LC_ALL=C tr -dc 'A-Za-z0-9!#%&\()*+,-./:;<=>?@[\]^_{}~' </dev/urandom | head -c 20)
+mysqlnextcloudpwd=$(LC_ALL=C tr -dc 'A-Za-z0-9!#%&\()*+,-./:;<=>?@[\]^_{}~' </dev/urandom | head -c 20)
 
 
 # Starting mysql container
