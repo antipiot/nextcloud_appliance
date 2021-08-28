ba1825f27d0cac987543c59e4bb87fb419074e2b
antipiot <jonassauge@gmail.com>
2021-08-28 18:26:56 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -20,7 +20,7 @@ mysqlrootpwd=test
 # Starting mysql container
 docker run -d --name $dbhostname --restart unless-stopped --user $uid:$uid -v $rootfolder/database:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$mysqlrootpwd -e MYSQL_DATABASE=$dbname -e MYSQL_USER=$nextcloud -e MYSQL_PASSWORD=$mysqlnextcloudpwd mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 # Starting nextcloud container
-docker run -d --name=nextcloud --restart unless-stopped -p $https:443 --link $dbhostname PUID=$uid PGID=$gid -e TZ=Europe/Geneva -v $rootdatafolderc/config:/config -v $rootdatafolder/data:/data linuxserver/nextcloud
+docker run -d --name=nextcloud --restart unless-stopped -p $https:443 --link $dbhostname -e PUID=$uid -e PGID=$gid -e TZ=Europe/Geneva -v $rootdatafolderc/config:/config -v $rootdatafolder/data:/data linuxserver/nextcloud
 # Starting updater container
 docker run -d --name watchtower --restart=unless-stopped -e WATCHTOWER_SCHEDULE="0 0 4 * * *" -e WATCHTOWER_CLEANUP="true" -e TZ="Europe/paris" -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
 echo "Database user: $dbusername \ 
