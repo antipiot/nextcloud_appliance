a6597e052e073b7c94cfbed6cae1d56191939d40
antipiot <jonassauge@gmail.com>
2021-08-28 13:06:56 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -25,15 +25,15 @@ mysqlnextcloudpwd=$(LC_ALL=C tr -dc 'A-Za-z0-9!#%&\()*+,-./:;<=>?@[\]^_{}~' </de
 
 
 # Starting mysql container
-docker run -d --name $dbhostname --user $uid:$uid -v $rootfolder/database:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$mysqlrootpwd -e MYSQL_DATABASE=$dbname -e MYSQL_USER=$nextcloud -e MYSQL_PASSWORD=$mysqlnextcloudpwd mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+docker run -d --name $dbhostname --user $uid:$uid -v $rootfolder/nextcloud/database:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$mysqlrootpwd -e MYSQL_DATABASE=$dbname -e MYSQL_USER=$nextcloud -e MYSQL_PASSWORD=$mysqlnextcloudpwd mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 # Starting nextcloud container
-docker run -d -p $https:443 -p $http:80 --name=nextcloud --link $dbhostname --restart unless-stopped -e TZ=Europe/Geneva -v $rootdatafolder/nextcoud/config:/config -v $rootdatafolder/nextcoud/data:/data -e MYSQL_DATABASE=$dbname -e MYSQL_HOST=$dbhostname -e MYSQL_USER=$nextcloud -e MYSQL_PASSWORD=$mysqlnextcloudpwd linuxserver/nextcloud
+docker run -d -p $https:443 -p $http:80 --name=nextcloud --link $dbhostname --restart unless-stopped -e TZ=Europe/Geneva -v $rootdatafolder/nextcloud/config:/config -v $rootdatafolder/nextcloud/data:/data -e MYSQL_DATABASE=$dbname -e MYSQL_HOST=$dbhostname -e MYSQL_USER=$nextcloud -e MYSQL_PASSWORD=$mysqlnextcloudpwd linuxserver/nextcloud
 # Starting updater container
 docker run -d --name watchtower --restart=unless-stopped -e WATCHTOWER_SCHEDULE="0 0 4 * * *" -e WATCHTOWER_CLEANUP="true" -e TZ="Europe/paris" -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower
-echo "Credentials: \
-Database hostname: $dbhostname \
-Database user: $dbusername \
+echo "Database user: $dbusername \
 Database password: $mysqlnextcloudpwd \
-Database root password: $mysqlrootpwd" > $rootfolder/credentials.txt
+Database hostname: $dbhostname \
+Database name: $dbname \ 
+Database hostname: $dbhostname" > $rootfolder/nextcloud/credentials.txt
 
-rm -f /etc/init.d/startup.sh
+
