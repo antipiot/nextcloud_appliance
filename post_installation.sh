b2f07225df8346fa5128da035f902a3e3e9aae5f
antipiot <jonassauge@gmail.com>
2021-08-28 11:05:48 +0200

    Update post_installation.sh

--- post_installation.sh
+++ post_installation.sh
@@ -4,6 +4,11 @@
 
 ## Installing sources, dependencies and updating host
 # Add Docker’s official GPG key:
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update -y
+apt install -y docker-ce docker-ce-cli containerd.io
 
 echo "@reboot bash /root/startup.sh" >> /etc/crontab
 # Install Docker along network tools
