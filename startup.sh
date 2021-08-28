ced65c36ecbe8112e7a4134b8ce4913db8ddc55a
antipiot <jonassauge@gmail.com>
2021-08-28 18:45:52 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -18,7 +18,7 @@ mysqlrootpwd=$(LC_ALL=C tr -dc 'A-Za-z0-9!#%&\()*+,-./:;<=>?@[\]^_{}~' </dev/ura
 
 
 # Starting mysql container
-docker run -d --name $dbhostname --restart unless-stopped --user $uid:$uid -v $rootfolder/database:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$mysqlrootpwd -e MYSQL_DATABASE=$dbname -e MYSQL_USER=$nextcloud -e MYSQL_PASSWORD=$mysqlnextcloudpwd mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+docker run -d --name $dbhostname --restart unless-stopped --user $uid:$uid -v $rootfolder/database:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$mysqlrootpwd -e MYSQL_DATABASE=$dbname -e MYSQL_USER=$dbusername -e MYSQL_PASSWORD=$mysqlnextcloudpwd mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 # Starting nextcloud container
 docker run -d --name=nextcloud --restart unless-stopped -p $https:443 --link $dbhostname -e PUID=$uid -e PGID=$gid -e TZ=Europe/Geneva -v $rootdatafolderc/config:/config -v $rootdatafolder/data:/data linuxserver/nextcloud
 # Starting updater container
