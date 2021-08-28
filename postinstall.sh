d7c34e3da02dd1614586cf4c368c8a87c26eb364
antipiot <jonassauge@gmail.com>
2021-08-28 18:13:11 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -15,5 +15,6 @@ apt install -y docker-ce docker-ce-cli containerd.io
 # Download reboot script and add run from rc.local
 wget -O /root/startup.sh https://raw.githubusercontent.com/antipiot/nextcloud_appliance/main/startup.sh
 chmod +x /root/startup.sh
+# Add script to be started on boot then remove it once ran
 echo "#!/bin/sh -e \n/root/startup.sh \nrm -f $0 \nexit 0" > /etc/rc.local
 chmod 755 /etc/rc.local
