efdf2a7899651bf08fca3086849aaf2b4e255467
antipiot <jonassauge@gmail.com>
2021-08-28 10:46:05 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,9 +2,6 @@
 
 # Jonas Sauge
 
-# apt-get update
-# apt install -y curl gpg
-
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # Setup Stable Docker repository
